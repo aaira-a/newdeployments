a3e614f73abb31df6bed42a5ab7e60071a46f373
azam alias <azam.alias@iproperty.com>
2015-05-18 17:28:25 +0800

    added function to get all pattern-matched keys from a bucket

--- mycleanup.py
+++ mycleanup.py
@@ -2,5 +2,9 @@
 import re
 
 
+def get_all_matching_keys(bucket, prefix_):
+    return [item for item in bucket.list(prefix=prefix_) if is_matching_versioned_pattern(item.key) is True]
+
+
 def is_matching_versioned_pattern(path):
     return bool(re.search('\/(.*-.*\..*$)', path))
--- test_mycleanup.py
+++ test_mycleanup.py
@@ -1,11 +1,16 @@
 import unittest
 
 from mycleanup import (
+    get_all_matching_keys,
     is_matching_versioned_pattern,
     )
 
 
-class VersionedPatternChecker(unittest.TestCase):
+import boto
+import moto
+
+
+class VersionedPatternCheckerTest(unittest.TestCase):
 
     def test_path_with_simple_dash_should_return_true(self):
         self.assertTrue(is_matching_versioned_pattern('/myfile-0001.js'))
@@ -21,3 +26,33 @@ class VersionedPatternChecker(unittest.TestCase):
 
     def test_path_without_dash_in_filename_should_return_false(self):
         self.assertFalse(is_matching_versioned_pattern('/folder/myfile0001.js'))
+
+
+class GetMatchingKeysIntegrationTest(unittest.TestCase):
+
+    @moto.mock_s3()
+    def test_get_matched_pattern_key_with_prefix_should_return_correct_item(self):
+        paths = ['prefix1/d/file1.css',
+                 'prefix1/d/file2.css',
+                 'prefix1/d/file3-v1.0.0.css',
+                 'prefix2/d/file4-123.js',
+                 'prefix2/d/file5.js',
+                 'prefix3/d/file6-23.png',
+                 'prefix3/d/file7.1.2.3.jpg']
+
+        conn = boto.connect_s3('key', 'secret')
+        bucket = conn.create_bucket('mybucket567')
+
+        for item in paths:
+            k = boto.s3.key.Key(bucket)
+            k.key = item
+            k.set_contents_from_string(item)
+
+        result1 = get_all_matching_keys(bucket, 'prefix1')
+        self.assertIn('prefix1/d/file3-v1.0.0.css', result1[0].key)
+
+        result2 = get_all_matching_keys(bucket, 'prefix2')
+        self.assertIn('prefix2/d/file4-123.js', result2[0].key)
+
+        result3 = get_all_matching_keys(bucket, 'prefix3')
+        self.assertIn('prefix3/d/file6-23.png', result3[0].key)
